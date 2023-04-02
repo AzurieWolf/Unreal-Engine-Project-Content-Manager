822300843673be26fd03acd6f0d20e6dddfb889c
AzurieWolf <AzurieWolf@outlook.com>
2023-04-02 17:02:28 -0300

    Renamed UEPCM to Data

--- UEPCM/Bin/jq-win64-load.bat
+++
@@ -1,7 +0,0 @@
-:: Update the JSON file with the selected directory
-set jq=UEPCM\Bin\jq-win64.exe
-if not exist "%jq%" (
-    echo Error: JSON processor not found at "%jq%".
-    pause
-    exit /b 1
-)
--- UEPCM/Bin/jq_functions.bat
+++
@@ -1,7 +0,0 @@
-:: Remove the selected project from the Projects.json file
-%jq% "del(.%selectedProject%)" "%ProjectsJsonData%" > "%ProjectsJsonData%.temp"
-move /y "%ProjectsJsonData%.temp" "%ProjectsJsonData%" > nul
-
-:: Append new setter and value to the Projects.json file
-%jq% --arg newProperty "newValue" ". + { "newProperty": $newProperty }" "%ProjectsJsonData%" > "%ProjectsJsonData%.temp"
-move /y "%ProjectsJsonData%.temp" "%ProjectsJsonData%" > nul
